8d9434e00879ad4eed9f0f95961fa9f440ee30a5
Megan Weijiang <meganweijiang@utexas.edu>
2018-11-15 19:19:17 -0600

    Add all project files

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
+import selectExpenses from '../selectors/expenses';
+
+const ExpenseList = (props) => (
+  <div>
+    <h1>Expense List</h1>
+    {props.expenses.map((expense) => {
+      return <ExpenseListItem key={expense.id} {...expense} />;
+    })}
+    <p>Total: {props.expenses.reduce((total, expense) => {
+      return total + expense.amount
+    }, 0)}</p>
+  </div>
+);
+
+const mapStateToProps = (state) => {
+  return {
+    expenses: selectExpenses(state.expenses, state.filters)
+  };
+};
+
+export default connect(mapStateToProps)(ExpenseList);
+
---
+++ src/playground/hoc.js
@@ -0,0 +1,38 @@
+// Higher order component (HOC) - A component (HOC) that renders another component
+// Reuse code
+// Render hijacking
+// Prop manipulation
+// Abstract state
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Info = (props) => (
+  <div>
+    <h1>Info</h1>
+    <p>The info is: {props.info}</p>
+  </div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      { props.isAdmin && <p>This is private info. Please don't share.</p>}
+      <WrappedComponent {...props}/>
+    </div>
+  );
+};
+
+const requireAuthentication = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      { props.isAuthenticated ? <WrappedComponent {...props}/> : <p>Please log in to see info.</p>}
+    </div>
+  );
+}
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={false} info="details" />, document.getElementById("app"));
+ReactDOM.render(<AuthInfo isAuthenticated={false} info="details" />, document.getElementById("app"));
